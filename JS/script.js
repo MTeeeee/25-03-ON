9fabd8053079912ea626937db56cf1e3dce00e9f
MTeeeee <112711304+MTeeeee@users.noreply.github.com>
2025-07-25 17:43:30 +0200

    JS + neue Links

---
+++ JS/script.js
@@ -0,0 +1,58 @@
+console.log("Hello, World!");
+console.log("Zweite Nachricht");
+console.log("Dritte Nachricht");
+
+
+var farbe = "rot";
+
+const meineZahl = 42;
+
+let meineZahl2 = 24;
+meineZahl2 = 12;
+meineZahl2 = 15;
+meineZahl2 = 105;
+
+console.log(meineZahl2);
+
+let counter = 0;
+counter ++;
+counter ++;
+counter ++;
+counter ++;
+console.log(counter);
+
+// snail_case
+// camelCase
+// PascalCase
+// kebab-case
+
+console.log(`meine Farbe = ${farbe}`)
+
+
+let myArray = [1, 2, 3, 4, 5];
+
+let myPop = myArray.pop();
+console.log(myArray);
+
+console.log(myPop);
+
+if (1 == "1") {
+    console.log("Bedingung erfüllt");
+}
+if (1 === "1") {
+    console.log("Bedingung nicht erfüllt");
+}
+
+if (1 != "1") {
+    console.log("Bedingung nicht erfüllt");
+}
+if (1 !== "1") {
+    console.log("Bedingung erfüllt");
+}
+
+
+function addiere(x, y) {
+    return x + y;
+}
+
+console.log(addiere(5, 10));
--- README.md
+++ README.md
@@ -3,3 +3,21 @@ fun Links:
 
 Identischer HTML Code sehr unterschiedliches CSS: 
 https://www.csszengarden.com/pages/alldesigns/
+
+
+https://cssbattle.dev/
+https://css-challenges.com/
+https://100dayscss.com/
+
+
+CSS Flexbox:
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+https://flexboxfroggy.com/
+
+CSS GRID:
+https://css-tricks.com/snippets/css/complete-guide-grid/
+https://cssgridgarden.com/#de
+
+
+https://getbootstrap.com/
+https://tailwindcss.com/
